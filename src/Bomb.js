1751cd71a0be07f2bc30b7fe35536166fa4412ad
Getu Gebre <getu.gebre.gg@gmail.com>
2021-02-03 17:52:43 -0600

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,29 @@
 // your Bomb code here!
+import React, {Component} from 'react'
+export default class Bomb extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            secondsLeft: props.initialCount 
+        }
+    } 
+    tick(a) {
+        return a--
+    }
+    decrement = () => {
+        let val = this.props.initialCount
+        
+        this.setState ({
+            secondsLeft: val
+        }) 
+    }
+
+    render() {
+        return (
+                // {this.decrement()}
+                <h1> {this.state.secondsLeft} seconds left before I go boom!</h1>
+            
+        )
+    } 
+
+}
